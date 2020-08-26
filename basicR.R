e2bac5bafa0ede04bd95a5f5ebc554061cb0ee67
Monica Murugesan <46895738+monicamurugesan@users.noreply.github.com>
2020-08-26 20:50:31 +0530

    Add files via upload
    Basic R codes.

---
+++ basicR.R
@@ -0,0 +1,34 @@
+1+1
+x<- 1+1
+x
+a <-50
+b <-20
+a*b+x
+a+b*x
+(6*3)+5/2
+exp(1)
+db <-c(1,2,4.5)
+db
+d <-(1,2,4)
+rep(1,10)
+rep(2,3)
+vect_x <-c(2,0,0,4)
+vect <-c(1,2,3,4)
+vec1 <-vect_x+vect
+vec1
+vec1*4
+vec1[1:4]
+vec1[2]=3
+vect_char <- c('R','python','java',10,100.5)
+vect2<-c('R','python','java',1)
+c <-c("a","b","c")
+c
+df <-data.frame(x=1:3,y=c("a","b","c"))
+df[1,1]
+df[1,]
+df1 <-data.frame(height=c(102,120),width=c(50,50))
+df1
+df1[1,1]
+df[c(1,3),2]
+df[c(1,3),1]
+df[c(1,3),1:2]
